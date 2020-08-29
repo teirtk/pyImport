08d99e40b92a165a298d835f7e369b145d386d90
teirtk <teirtboo@gmail.com>
2020-08-29 17:02:13 +0700

    update

--- caytrong.py
+++ caytrong.py
@@ -5,7 +5,7 @@ from datetime import timedelta, date
 import pandas as pd
 import numpy as np
 from xlrd import biffh
-from fuzzywuzzy import fuzz, process
+from fuzzywuzzy import process
 import config
 
 
@@ -108,7 +108,7 @@ def do_process(file, conn):
             names.append(name)
     choices = process.extract(mota2, config.town_list.keys(), limit=10)
     max = 0
-    max_name = ""
+    mota2 = ""
     for name, ratio in choices:
         total = 0
         for subname in names:
@@ -117,88 +117,62 @@ def do_process(file, conn):
         average = total / len(names)
         if max < average:
             max = average
-            max_name = name
-    print(max_name, max)
-    return ""
-
-
-"""
-def do_process(file, conn):
-    basename = os.path.basename(file)
-    buffer = io.StringIO()
-    header = True
+            mota2 = name
+    for idx, name in enumerate(names):
+        (s, _) = process.extractOne(subname, config.town_list[name])
+        if s:
+            names[idx] = s
     try:
-        with pd.ExcelFile(file) as xls:
-            for idx, name in enumerate(xls.sheet_names):
-                df = pd.read_excel(xls, sheet_name=name, encoding='utf-8')
-                if not idx:
-                    (prefix, mota2) = fix_addr(name)
-                    if not mota2:
-                        return f"{basename}: Không thể lấy được tên huyện \n"
-                    fdate = get_date(df.head(20))
-                    if fdate is None:
-                        return f"{basename}: Không lấy được ngày tháng \n"
-                    continue
-                (_, mota1) = fix_addr(name, mota2)
-                if not mota1:
-                    continue
-                col2 = get_col(df)
-                df = df.reindex(
-                    ["Unnamed: 1", col2], axis="columns")
-                first_row = get_first_row(df['Unnamed: 1'])
-                if first_row < 0:
-                    continue
-                df = df.loc[first_row:, :]
-                df = df.dropna(subset=["Unnamed: 1"]).reset_index(drop=True)
-                df['Unnamed: 1'] = df['Unnamed: 1'].astype(str).str.lower().replace(
-                    config.my_dict['caytrong'], regex=True).str.strip().str.capitalize()
-                df[col2] = df[col2].astype(str).replace(
-                    {r'[A-Za-z]+': '', r'\s+': ''}, regex=True)
-                df['dup'] = df.duplicated(['Unnamed: 1'], keep=False)
-                df.loc[df['Unnamed: 1'] == 'Đậu các loại', 'dup'] = False
-                df.loc[df['Unnamed: 1'] == "Khác", 'dup'] = False
-                df[col2] = pd.to_numeric(
-                    df[col2], errors='coerce').round(2).apply(str)
-                df['nhom'] = df['Unnamed: 1'].str.strip().where(
-                    df['Unnamed: 1'].isin(keyword) & ~df['dup'], np.nan).fillna(method='ffill')
-                df['thuoctinhlb'] = df['Unnamed: 1'].where(
-                    ~df['dup']).fillna(method='ffill')
-                df = df[~df['thuoctinhlb'].astype(
-                    str).str.contains('Cây rau, màu', na=False)]
-                df = df[df['dup'] & df[col2].astype(float).gt(0)]
-                df['thuoctinh'] = '"' + \
-                    df['thuoctinhlb'].apply(str).str.strip() + '":' + df[col2]
-                df.rename(columns={"Unnamed: 1": 'chuyenmuc'}, inplace=True)
-                df["nhom"] = df["nhom"].apply(str).replace({'Đậu xanh': 'Đậu'})
-                dfp = df.groupby(["nhom", "chuyenmuc"]).agg(
-                    {"thuoctinh": ",".join})
-                dfp['thuoctinh'] = "{" + dfp['thuoctinh'] + "}"
-                dfp["fdate"] = fdate
-                dfp["mota1"] = mota1
-                dfp["mota2"] = f"{prefix} {mota2}"
-                dfp.to_csv(buffer, header=header)
-                if header:
-                    header = False
-        if not buffer.getvalue().count('\n'):
-            return f"{basename}: Sai định dạng \n"
-        buffer.seek(0)
-        with conn.cursor() as cur:
-            cur.execute(f"CREATE TEMP TABLE tmp_table ON COMMIT DROP AS "
-                        f"TABLE {config.ext['caytrong']['table']} WITH NO DATA")
-            cur.copy_expert(
-                "COPY tmp_table FROM STDIN WITH CSV HEADER", buffer)
-            cur.execute(f"INSERT INTO {config.ext['caytrong']['table']} "
-                        f"SELECT * FROM tmp_table EXCEPT "
-                        f"SELECT * FROM {config.ext['caytrong']['table']};")
-            nline = cur.rowcount
-        conn.commit()
-        if nline:
-            return f"{basename}: {nline:,} dòng được thêm \n"
-        return f"{basename}: Dữ liệu đã có (bỏ qua) \n"
+        for idx, df in enumerate(dfa):
+            df = df.dropna(subset=["Unnamed: 1"]).reset_index(drop=True)
+            df['Unnamed: 1'] = df['Unnamed: 1'].astype(str).str.lower().replace(
+                config.my_dict['caytrong'], regex=True).str.strip().str.capitalize()
+            df[col2] = df[col2].astype(str).replace(
+                {r'[A-Za-z]+': '', r'\s+': ''}, regex=True)
+            df['dup'] = df.duplicated(['Unnamed: 1'], keep=False)
+            df.loc[df['Unnamed: 1'] == 'Đậu các loại', 'dup'] = False
+            df.loc[df['Unnamed: 1'] == "Khác", 'dup'] = False
+            df[col2] = pd.to_numeric(
+                df[col2], errors='coerce').round(2).apply(str)
+            df['nhom'] = df['Unnamed: 1'].str.strip().where(
+                df['Unnamed: 1'].isin(keyword) & ~df['dup'], np.nan).fillna(method='ffill')
+            df['thuoctinhlb'] = df['Unnamed: 1'].where(
+                ~df['dup']).fillna(method='ffill')
+            df = df[~df['thuoctinhlb'].astype(
+                str).str.contains('Cây rau, màu', na=False)]
+            df = df[df['dup'] & df[col2].astype(float).gt(0)]
+            df['thuoctinh'] = '"' + \
+                df['thuoctinhlb'].apply(str).str.strip() + '":' + df[col2]
+            df.rename(columns={"Unnamed: 1": 'chuyenmuc'}, inplace=True)
+            df["nhom"] = df["nhom"].apply(str).replace({'Đậu xanh': 'Đậu'})
+            dfp = df.groupby(["nhom", "chuyenmuc"]).agg(
+                {"thuoctinh": ",".join})
+            dfp['thuoctinh'] = "{" + dfp['thuoctinh'] + "}"
+            dfp["fdate"] = fdate
+            dfp["mota1"] = names[idx]
+            dfp["mota2"] = mota2
+            dfp.to_csv(buffer, header=header)
+            if header:
+                header = False
+            if not buffer.getvalue().count('\n'):
+                return f"{basename}: Sai định dạng \n"
+            buffer.seek(0)
+            with conn.cursor() as cur:
+                cur.execute(f"CREATE TEMP TABLE tmp_table ON COMMIT DROP AS "
+                            f"TABLE {config.ext['caytrong']['table']} WITH NO DATA")
+                cur.copy_expert(
+                    "COPY tmp_table FROM STDIN WITH CSV HEADER", buffer)
+                cur.execute(f"INSERT INTO {config.ext['caytrong']['table']} "
+                            f"SELECT * FROM tmp_table EXCEPT "
+                            f"SELECT * FROM {config.ext['caytrong']['table']};")
+                nline = cur.rowcount
+            conn.commit()
+            if nline:
+                return f"{basename}: {nline:,} dòng được thêm \n"
+            return f"{basename}: Dữ liệu đã có (bỏ qua) \n"
     except biffh.XLRDError:
         return f"{basename}: bị bảo vệ\n"
     except TypeError:
         return f"{basename}: Sai định dạng ở sheet {name}\n"
     finally:
         buffer.close()
-"""
